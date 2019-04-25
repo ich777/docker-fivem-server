0f5ec2ae02892e426a05fc5e5bb13ac030abaae3
ich777 <christophhummer@gmail.com>
2019-04-26 00:31:46 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sleep infinity
 <TODO>
+
+
+CUR_V="$(wget -q -O - ${SRV_ADR} | grep 'Parent directory/</a></td><td>-</td><td' | head | grep -Po '(?<=href=").{45}' |grep 1)"
+wget -qi - ${SRV_ADR}$CUR_V/fx.tar.xz
