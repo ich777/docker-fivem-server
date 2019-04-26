71f8bb1757fde5f474177ff19fd2974a5cc59279
ich777 <christophhummer@gmail.com>
2019-04-26 10:34:09 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -5,14 +5,14 @@ LAT_V="$(wget -q -O - ${SRV_ADR} | grep 'Parent directory/</a></td><td>-</td><td
 if [ -z "$CUR_V" ]; then
   echo "---FiveM not found!---"
   cd ${SERVER_DIR}
-  wget -O $LAT_V "${SRV_ADR}$LAT_V/fx.tar.xz"
+  wget -qO $LAT_V "${SRV_ADR}$LAT_V/fx.tar.xz"
   tar -xf $LAT_V
   mv ${SERVER_DIR}/$LAT_V ${DATA_DIR}/fx.tar.xz-$LAT_V
 elif [ "$LAT_V" != "$CUR_V" ]; then
   echo "---Newer version found, installing!---"
   rm ${DATA_DIR}/fx.tar.xz-$CUR_V
   cd ${SERVER_DIR}
-  wget -O $LAT_V "${SRV_ADR}$LAT_V/fx.tar.xz"
+  wget -qO $LAT_V "${SRV_ADR}$LAT_V/fx.tar.xz"
   tar -xf $LAT_V
   mv ${SERVER_DIR}/$LAT_V ${DATA_DIR}/fx.tar.xz-$LAT_V
 elif [ "$LAT_V" == "$CUR_V" ]; then
@@ -25,13 +25,13 @@ fi
 if [ ! -d "${SERVER_DIR}/cfx-server-data" ]; then
   echo "---CFX-SERVER-DATA not found, downloading...---"
   cd ${SERVER_DIR}
-  wget -O server-data.zip "http://github.com/citizenfx/cfx-server-data/archive/master.zip"
+  wget -qO server-data.zip "http://github.com/citizenfx/cfx-server-data/archive/master.zip"
   unzip server-data.zip
   rm ${SERVER_DIR}/server-data.zip
   mv ${SERVER_DIR}/cfx-server-data-master ${SERVER_DIR}/cfx-server-data
 else
   echo "---CFX-SERVER-DATA found, updating...---"
-  wget -O server-data.zip "http://github.com/citizenfx/cfx-server-data/archive/master.zip"
+  wget -qO server-data.zip "http://github.com/citizenfx/cfx-server-data/archive/master.zip"
   unzip server-data.zip
   rm ${SERVER_DIR}/server-data.zip
   mv ${SERVER_DIR}/cfx-server-data-master ${SERVER_DIR}/cfx-server-data
@@ -46,6 +46,6 @@ chmod -R 770 ${DATA_DIR}
 
 
 cd ${SERVER_DIR}
-./run.sh +exec server.cfg +sv_licenseKey ${SERVER_KEY} +sv_hostname '"'$HOSTNAME'"' +rcon_password ${RCON_PWD}
+./run.sh +exec server.cfg +sv_licenseKey ${SERVER_KEY} +sv_hostname $HOSTNAME +rcon_password ${RCON_PWD}
 
 sleep infinity
