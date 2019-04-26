abe0acd83bc9f7348c0c2755b7502f0d85168603
ich777 <christophhummer@gmail.com>
2019-04-26 09:51:48 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
-sleep infinity
-<TODO>
+CUR_V="$(find -name fx.tar.xz-[^extended]* | cut -d '-' -f 2,3)"
+LAT_V="$(wget -q -O - ${SRV_ADR} | grep 'Parent directory/</a></td><td>-</td><td' | head | grep -Po '(?<=href=").{45}' |grep 1)"
+
+if [ -z "$CUR_V" ]; then
+  echo "---FiveM not found!---"
+  cd ${SERVER_DIR}
+  wget -O $LAT_V "${SRV_ADR}$LAT_V/fx.tar.xz"
+  tar -xf $LAT_V
+  mv ${SERVER_DIR}/$LAT_V ${DATA_DIR}/fx.tar.xz-$LAT_V
+elif [ "$LAT_V" != "$CUR_V" ]; then
+  echo "---Newer version found, installing!---"
+  rm ${DATA_DIR}/fx.tar.xz-$CUR_V
+  cd ${SERVER_DIR}
+  wget -O $LAT_V "${SRV_ADR}$LAT_V/fx.tar.xz"
+  tar -xf $LAT_V
+  mv ${SERVER_DIR}/$LAT_V ${DATA_DIR}/fx.tar.xz-$LAT_V
+elif [ "$LAT_V" == "$CUR_V" ]; then
+  echo "---Teeworlds Version up-to-date---"
+else
+  echo "---Something went wrong, putting server in sleep mode---"
+  sleep infinity
+fi
+
+if [ ! -d ${SERVER_DIR}/cfx-server-data ]; then
+  echo "---CFX-SERVER-DATA not found, downloading...---"
+  cd ${SERVER_DIR}
+  wget -O server-data.zip "http://github.com/citizenfx/cfx-server-data/archive/master.zip"
+  unzip server-data.zip
+  rm ${SERVER_DIR}/server-data.zip
+  mv ${SERVER_DIR}/cfx-server-data-master ${SERVER_DIR}/cfx-server-data
+else
+  echo "---CFX-SERVER-DATA found, updating...---"
+  wget -O server-data.zip "http://github.com/citizenfx/cfx-server-data/archive/master.zip"
+  unzip server-data.zip
+  rm ${SERVER_DIR}/server-data.zip
+  mv ${SERVER_DIR}/cfx-server-data-master ${SERVER_DIR}/cfx-server-data
+fi
+
+if [ ! -f ${SERVER_DIR}/server.cfg ]; then
+   cd ${SERVER_DIR}
+   wget -qi server.cfg "https://raw.githubusercontent.com/ich777/docker-fivem-server/master/configs/server.cfg"
+fi
+chmod -R 770 ${DATA_DIR}
 
 
-CUR_V="$(wget -q -O - ${SRV_ADR} | grep 'Parent directory/</a></td><td>-</td><td' | head | grep -Po '(?<=href=").{45}' |grep 1)"
-wget -qi - ${SRV_ADR}$CUR_V/fx.tar.xz
+cd ${SERVER_DIR}
+./run.sh +exec server.cfg +sv_licenseKey ${SERVER_KEY} +sv_hostname '"'$HOSTNAME'"' +rcon_password ${RCON_PWD}
