cf693334fb359970f5d302f3cea1b896dab84449
ich777 <christophhummer@gmail.com>
2019-04-28 18:33:42 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -41,6 +41,7 @@ fi
 chmod -R 770 ${DATA_DIR}
 
 echo "---Starting Server---"
+cd ${SERVER_DIR}
 exec ${SERVER_DIR}/run.sh +exec ${GAME_CONFIG} +sv_licenseKey $SERVER_KEY +sv_hostname ${SRV_NAME} ${START_VARS}
 
 sleep infinity
