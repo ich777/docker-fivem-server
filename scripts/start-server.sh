fa8a6c0c6d3ea2d6fae89a509f5d55b37c6a117a
ich777 <christophhummer@gmail.com>
2019-04-28 17:59:02 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-sleep infinity
-
 CUR_V="$(find -name fx.tar.xz-[^extended]* | cut -d '-' -f 2,3)"
 LAT_V="$(wget -q -O - ${SRV_ADR} | grep 'Parent directory/</a></td><td>-</td><td' | head | grep -Po '(?<=href=").{45}' |grep 1)"
 
